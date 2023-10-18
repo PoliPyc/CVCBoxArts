18e31ab02a7787e5e1f008b979255c5c5de319c4
poliakustyczny <poliakustyczny@gmail.com>
2023-10-18 11:30:46 +0200

    working image creation and saving

--- app/main.py
+++ app/main.py
@@ -1,33 +1,46 @@
+import base64
+import os
+from io import BytesIO
 
 import PySimpleGUI as sg
 from PIL import Image, ImageTk
-import os
 
-import base64
-from io import BytesIO
+USBLOADER_COVER_WIDTH = 160
+
+home_catalog = os.path.expanduser("~") + "/projects/CVCBoxArts/input"
+filetypes = (("all images", "*.png *.jpg *.jpeg"),)
+gametypes = ("NES", "SNES", "N64", "GENESIS", "TGX16")
+boxtypes = ("Front", "Back", "Full", "3D")
 
 
-
-home_catalog = os.path.expanduser('~') + "/projects/CVCBoxArts/input"
-filetypes = (('all images', '*.png *.jpg *.jpeg'),)
-gametypes = ('NES', 'SNES', 'N64', 'GENESIS', 'TGX16')
-boxtypes = ('Front', 'Back', 'Full', '3D')
-
-class BoxArt():
-    def __init__(self, cover_path: str = None, gametype: str = None, boxtype: str = None) -> None:
+class BoxArt:
+    def __init__(
+        self, cover_path: str = None, gametype: str = None, boxtype: str = None, name: str = None
+    ) -> None:
         self.cover_path = cover_path
         self.gametype = gametype
         self.boxtype = boxtype
-        self.file = None
-    
+        self.name = None
+        self.boxart = None
+
     def generate_new_boxart(self) -> Image:
+        def resize_to_width(image, width):
+            wpercent = (width/float(image.size[0]))
+            hsize = int((float(image.size[1])*float(wpercent)))
+            return image.resize((width,hsize), Image.LANCZOS)
+
         cover = Image.open(self.cover_path)
-        banner = Image.open('/home/poli/projects/CVCBoxArts/data/banner_front_nes.png')
-        boxart = cover.copy()
-        boxart.paste(banner)
-        boxart.save('/home/poli/projects/CVCBoxArts/output/test.png', quality=95)
-        print('zapisalo sie')
-        return boxart
+        banner = Image.open("/home/poli/projects/CVCBoxArts/data/banner_front_nes.png")
+        self.boxart = cover.copy()
+        self.name = cover.filename
+        if banner.width > self.boxart.width:
+            print('Resizing banner')
+            banner = resize_to_width(banner, self.boxart.width)
+        self.boxart.paste(banner, (0,0), banner)
+        self.boxart = resize_to_width(self.boxart, USBLOADER_COVER_WIDTH)
+        # boxart.save("/home/poli/projects/CVCBoxArts/output/test.png", quality=95)
+        # print("zapisalo sie")
+        return self.boxart
 
     @staticmethod
     def convert_image_to_base64(image) -> str:
@@ -35,30 +48,59 @@ class BoxArt():
         image.save(buffered, format="JPEG")
         return base64.b64encode(buffered.getvalue())
 
+
 def run():
-    
     sg.theme("DarkAmber")
     layout = [
-        [[sg.Text("Select cover: ")] + [sg.FileBrowse(initial_folder=home_catalog, file_types=filetypes, enable_events=True, key="selected_cover", target=(0, 1))]],
-        [[sg.Text("Select gametype: ")] + [sg.OptionMenu(gametypes, default_value=gametypes[0], key='gametype')]],
-        [[sg.Text("Select boxtype: ")] + [sg.OptionMenu(boxtypes, default_value=boxtypes[0], key='boxtype')]],
+        [
+            [sg.Text("Select cover: ")]
+            + [
+                sg.FileBrowse(
+                    initial_folder=home_catalog,
+                    file_types=filetypes,
+                    enable_events=True,
+                    key="selected_cover",
+                    target=(0, 1),
+                )
+            ]
+        ],
+        [
+            [sg.Text("Select gametype: ")]
+            + [sg.OptionMenu(gametypes, default_value=gametypes[0], key="gametype")]
+        ],
+        [
+            [sg.Text("Select boxtype: ")]
+            + [sg.OptionMenu(boxtypes, default_value=boxtypes[0], key="boxtype")]
+        ],
         [sg.Text("Preview:")],
         [sg.Image(key="preview_image")],
         [sg.Button("Save"), sg.Button("Cancel")],
-        [sg.StatusBar("code: polipyc, graphics: krohmal © 2023")]
+        [sg.StatusBar("code: polipyc, graphics: krohmal © 2023")],
     ]
 
     window = sg.Window("CVCBoxArts v.0.1", layout, finalize=True)
 
     while True:
         event, values = window.read()
-        if ( event == "selected_cover"):
-            print('wybrano okładkę')
+        if event == "selected_cover":
+            print("wybrano okładkę")
             print(values)
-            new_boxart = BoxArt(values['selected_cover'], values['gametype'], values['boxtype'])
+            new_boxart = BoxArt(
+                values["selected_cover"], values["gametype"], values["boxtype"]
+            )
             data = ImageTk.PhotoImage(new_boxart.generate_new_boxart())
             print(data)
-            window['preview_image'].update(data=data)
+            window["preview_image"].update(data=data)
+        if event == "Save":
+            filename = sg.tk.filedialog.asksaveasfilename(
+                defaultextension='png',
+                filetypes=filetypes,
+                initialdir=home_catalog,
+                initialfile=new_boxart.name,             # Option added here
+                parent=window.TKroot,
+                title="Save As"
+            )
+            new_boxart.boxart.save(filename)
         if (
             event == sg.WIN_CLOSED or event == "Cancel"
         ):  # if user closes window or clicks cancel
