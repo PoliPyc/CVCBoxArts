2dee72b9ee2bea21b37ca03b0e76528627327b95
poliakustyczny <poliakustyczny@gmail.com>
2025-07-09 22:58:11 +0200

    slight refactor added fit to height

--- app/main.py
+++ app/main.py
@@ -1,12 +1,23 @@
+"""Custom boxart generator for Virtual Console injections designed for use with USBLoaderGX Cover system."""
+
+
 import base64
 import os
 from io import BytesIO
 from pathlib import Path
 
-import PySimpleGUI as sg
+import FreeSimpleGUI as sg
 from PIL import Image, ImageTk
 
-USBLOADER_COVER_WIDTH = 160
+__VERSION = "0.2.2"
+
+USBLOADER_COVER_FRONT_WIDTH = 160
+USBLOADER_COVER_FRONT_HEIGHT = 224
+
+FIT_OPTIONS = [
+    {"text": "fit to width", "key": "fit_w", "value": "width"},
+    {"text": "fit to height", "key": "fit_h", "value": "height"},
+]
 
 root_path = Path(__file__).resolve().parent
 data_path = Path(str(root_path) + "/../data").resolve()
@@ -17,46 +28,88 @@ boxtypes = ("Front", "Back", "Full", "3D")
 
 
 class BoxArt:
+    """Class for creating and processing game box art with banners."""
+
     def __init__(
         self,
         cover_path: str = None,
         gametype: str = None,
         boxtype: str = None,
+        fit: str = None,
         name: str = None,
     ) -> None:
+        """Initialize a BoxArt object with the given parameters.
+
+        Args:
+            cover_path: Path to the cover image file.
+            console_type: Game console type (e.g., NES, SNES).
+            box_type: Box art type (e.g., Front, Back).
+            fit: Scaling mode (width or height).
+        """
         self.cover_path = cover_path
         self.gametype = gametype
         self.boxtype = boxtype
+        self.fit = fit
         self.name = None
         self.boxart = None
+        self.cover = Image.new(
+            "RGB", (USBLOADER_COVER_FRONT_WIDTH, USBLOADER_COVER_FRONT_HEIGHT)
+        )
 
-    def generate_new_boxart(self) -> Image:
+    def generate_new_boxart(self) -> Image.Image:
+        """Create a new box art image with an overlaid banner.
+
+        Returns:
+            PIL.Image object with the processed box art or None if an error occurs.
+
+        Raises:
+            ValueError: If input parameters are invalid.
+            FileNotFoundError: If cover or banner file is not found.
+        """
+
         def resize_to_width(image, width):
             wpercent = width / float(image.size[0])
             hsize = int((float(image.size[1]) * float(wpercent)))
             return image.resize((width, hsize), Image.LANCZOS)
 
+        def resize_to_height(image, height):
+            hpercent = height / float(image.size[1])
+            wsize = int((float(image.size[0]) * float(hpercent)))
+            return image.resize((wsize, height), Image.LANCZOS)
+
         try:
-            cover = Image.open(self.cover_path)
+            if not self.cover_path or not Path(self.cover_path).is_file():
+                raise ValueError("Invalid or missing cover image path")
+            if self.gametype not in gametypes:
+                raise ValueError(f"Invalid console type: {self.gametype}")
+            if self.boxtype not in boxtypes:
+                raise ValueError(f"Invalid box type: {self.boxtype}")
+            # if self.fit not in FIT_OPTIONS:
+            # raise ValueError(f"Invalid fit option: {self.fit}")
+            self.boxart = Image.open(self.cover_path)
+
+        except (ValueError, FileNotFoundError) as e:
+            sg.popup_error(f"Error creating box art: {e}", title="Error")
+            return None
         except Exception as e:
-            print(e)
-            return
+            sg.popup_error(f"Unexpected error: {e}", title="Error")
+            return None
 
         banner = self._get_banner()
-        self.boxart = cover.copy()
-        self.name = cover.filename
-        if banner.width > self.boxart.width:
-            print("Resizing banner")
-            banner = resize_to_width(banner, self.boxart.width)
-        else:
-            print("resizing cover")
-            self.boxart = resize_to_width(self.boxart, banner.width)
+        self.name = self.boxart.filename
+        print("Resizing banner")
+        banner = resize_to_width(banner, USBLOADER_COVER_FRONT_WIDTH)
+        print("resizing cover")
+        if self.fit == "width":
+            self.boxart = resize_to_width(self.boxart, USBLOADER_COVER_FRONT_WIDTH)
+        elif self.fit == "height":
+            self.boxart = resize_to_height(self.boxart, USBLOADER_COVER_FRONT_HEIGHT)
 
-        self.boxart.paste(banner, (0, 0), banner)
-        self.boxart = resize_to_width(self.boxart, USBLOADER_COVER_WIDTH)
+        self.cover.paste(self.boxart, (0, 0))
+        self.cover.paste(banner, (0, 0), banner)
         # boxart.save("/home/poli/projects/CVCBoxArts/output/test.png", quality=95)
         # print("zapisalo sie")
-        return self.boxart
+        return self.cover
 
     def _get_banner(self) -> Image:
         return Image.open(
@@ -70,9 +123,13 @@ class BoxArt:
         return base64.b64encode(buffered.getvalue())
 
 
-def run():
-    sg.theme("DarkTeal11")
-    layout = [
+def create_layout() -> list:
+    """Create the GUI layout for the application.
+
+    Returns:
+        List defining the GUI layout.
+    """
+    return [
         [
             [sg.Text("Select cover: ", key="cover_label")]
             + [
@@ -97,12 +154,25 @@ def run():
                 )
             ]
         ],
+        # TODO:
         [sg.Text("Select boxtype: ")],
         [
             [sg.Checkbox("Front", default=True, key="box-front")]
-            + [sg.Checkbox("Back", key="box-back")]
-            + [sg.Checkbox("Full", key="box-full")]
-            + [sg.Checkbox("3D", key="box-3d")]
+            #     + [sg.Checkbox("Back", key="box-back")]
+            #     + [sg.Checkbox("Full", key="box-full")]
+            #     + [sg.Checkbox("3D", key="box-3d")]
+        ],
+        [
+            [
+                sg.Radio(
+                    option["text"],
+                    "fit",
+                    default=(i == 0),
+                    enable_events=True,
+                    key=option["key"],
+                )
+            ]
+            for i, option in enumerate(FIT_OPTIONS)
         ],
         [sg.Text("Preview:")],
         [sg.Image(key="preview_image")],
@@ -116,17 +186,27 @@ def run():
         ],
     ]
 
-    window = sg.Window("CVCBoxArts v.0.2", layout, finalize=True)
+
+def run():
+    sg.theme("DarkTeal11")
+
+    window = sg.Window("CVCBoxArts v." + __VERSION, create_layout(), finalize=True)
 
     while True:
         event, values = window.read()
         window["cover_label"].expand(expand_x=True)  # broken
-        if event in ("selected_cover", "gametype"):
+        print(event)
+        if event in ("selected_cover", "gametype") or event.startswith("fit_"):
             print("wybrano okładkę")
             print(values)
             if values["box-front"]:
-                boxtypes = "front"
-            new_boxart = BoxArt(values["selected_cover"], values["gametype"], boxtypes)
+                boxtypes = "Front"
+            for option in FIT_OPTIONS:
+                if values[option["key"]]:
+                    fit = option["value"]
+            new_boxart = BoxArt(
+                values["selected_cover"], values["gametype"], boxtypes, fit
+            )
             try:
                 data = ImageTk.PhotoImage(new_boxart.generate_new_boxart())
                 window["preview_image"].update(data=data)
